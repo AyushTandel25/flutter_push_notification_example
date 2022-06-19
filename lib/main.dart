861fcc346ab9b932bd3cc1a1c11d3c3882ade774
DESKTOP-D3675O5\ayush <ayush.tandel@sharpsell.ai>
2022-06-19 13:58:30 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_badger/flutter_app_badger.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  firebaseMessagingInitialization();
+  runApp(const MyApp());
+}
+
+
+ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterAppBadger.updateBadgeCount(1);
+
+  print(message.data);
+}
+
+firebaseMessagingInitialization() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      FlutterAppBadger.updateBadgeCount(1);
+      print('Message also contained a notification: ${message.notification!.title}');
+    }
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    FlutterAppBadger.removeBadge();
+  });
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Push Notification"),
+      ),
+      body: const Center(
+        child: Text(
+          "Notification Example"
+        ),
+      ),
+    );
+  }
+}
